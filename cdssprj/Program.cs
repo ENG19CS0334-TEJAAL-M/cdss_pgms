e99b682d8684cc7e343a67a76adddc5145f13875
ENG19CS0334-TEJAAL-M <eng19cs0334.tejaalm@gmail.com>
2022-05-02 14:46:19 +0530

    included a basic expression

--- cdssprj/Program.cs
+++ cdssprj/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace cdss
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hello World");
+            while(true)
+            {
+                Console.Write("> ");
+                var line=Console.ReadLine();
+                if(string.IsNullOrWhiteSpace(line))
+                    return;
+                if(line=="1+2*3")
+                    Console.WriteLine("7");
+                else
+                Console.WriteLine("error:invalid expression");
+            }
+        }
+    }
+}
