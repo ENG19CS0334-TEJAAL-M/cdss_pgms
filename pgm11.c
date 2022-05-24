5a2cf02f9f9026646ee9426872a43a99699216fa
Tejaal M <75902517+ENG19CS0334-TEJAAL-M@users.noreply.github.com>
2022-05-24 07:42:18 +0530

    Add files via upload

---
+++ pgm11.c
@@ -0,0 +1,26 @@
+// // for loop
+// #include<stdio.h>
+// #include<time.h>
+// int main()
+// {
+//     int i,fact=1,number;
+//     clock_t t;
+//     t=clock();
+//     printf("enter a number ");
+//     scanf("%d",&number);
+
+// for(i=1;i<=number;i++)
+// {
+//     fact=fact*i;
+// }
+// t=clock()-t;
+// double time_taken=((double)t)/CLOCKS_PER_SEC;
+
+// printf("Factorial of %d is %d \n",number,fact);
+// printf("time taken to execute = %f \n",time_taken);
+// return 0;
+// }
+
+//recursion
+#include<stdio.h>
+long fact
